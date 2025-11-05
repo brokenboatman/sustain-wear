faae05fe44633e42b077725368484ccbaa19bf16
Alistair Craig <alistaircraig393@gmail.com>
2025-11-05 15:34:05 +0000

    hope the file exists

---
+++ src/Components/Sidebar.vue
@@ -0,0 +1,16 @@
+<template>
+  <USlideover title="" close-icon="i-lucide-arrow-right">
+    <UButton label="Open" color="neutral" variant="subtle" />
+
+    <template #body>
+        <!-- <Placeholder class="h-full" /> -->
+        <ULink to="/docs/components/link" class="text-2xl">Link</ULink>
+        <br></br><br></br>
+        <ULink to="/docs/components/link" class="text-2xl">Link</ULink>
+        <br></br><br></br>
+        <ULink to="/docs/components/link" class="text-2xl">Link</ULink>
+        <br></br><br></br>
+        <ULink to="/docs/components/link" class="text-2xl">Link</ULink>
+    </template>
+  </USlideover>
+</template>
