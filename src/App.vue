ee257ba3e995e22555c0847d0ce0e878bc9d29e6
NoelKBoyd <noel.boyd01@gmail.com>
2025-11-05 16:13:24 +0000

    Fix for Alistair

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import { RouterLink, RouterView } from "vue-router";
   <UApp>
     <RouterLink to="/register" />
     <RouterLink to="/login" />
+    <RouterLink to="/account" />
     <RouterView />
   </UApp>
 </template>
