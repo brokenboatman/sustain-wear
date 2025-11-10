33bd9d0c9a10b4613b8087608ea47aabe124b0a2
Fleester <reece.g1611@gmail.com>
2025-11-10 23:51:14 +0000

    completed with dummy data

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,17 +1,20 @@
 <template>
-  <Bar
-    id="my-chart-id"
-    :options="chartOptions"
-    :data="chartData"
-    class="bar"
-  />
+  <div class="bar">
+    <h2>Your Monthly CO2 Savings (kg)</h2>
+    <Bar
+      id="my-chart-id"
+      :options="chartOptions"
+      :data="chartData"
+      class="bar"
+    />
+  </div>
 </template>
 
 <script>
 import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { Chart as ChartJS, Title, Tooltip, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+ChartJS.register(Title, Tooltip, BarElement, CategoryScale, LinearScale)
 
 export default {
   name: 'BarChart',
@@ -25,7 +28,7 @@ export default {
       chartData: {
         labels: [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ],
         datasets: [ {
-            label: 'CO2 Savings',
+            label: 'CO2 Saving (kg)',
             data: [40, 20, 12, 35, 50, 14, 26, 52, 41, 35, 7, 22],
             backgroundColor: "#00c15f",
             
@@ -42,5 +45,9 @@ export default {
 <style>
     .bar {
         background-color: white;
+        color: black;
+        text-align: center;
+        font-weight: 700;
+        font-size: 25px;
     }
 </style>
