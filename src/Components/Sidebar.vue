e8246c65450f10d5cee3be76e3d7eea0a271f4c2
Alistair Craig <alistaircraig393@gmail.com>
2025-11-11 17:40:42 +0000

    added an animation to the icons delaying how they appear

--- src/Components/Sidebar.vue
+++ src/Components/Sidebar.vue
@@ -3,23 +3,23 @@
     <UButton label="Open" color="neutral" variant="subtle" />
 
     <template #body>
-         <div class="flex items-center gap-4 mb-4">
+         <div class="flex items-center gap-4 mb-4 animate-slide-in" style="animation-delay: 0s">
             <UIcon name="line-md:heart" class="size-8" />
             <ULink to="/" class="text-2xl">My Donations</ULink>
          </div>
-         <div class="flex items-center gap-4 mb-4">
+         <div class="flex items-center gap-4 mb-4 animate-slide-in" style="animation-delay: 0.2s">
             <UIcon name="line-md:account" class="size-8" />
             <ULink to="/account" class="text-2xl">Account</ULink>
           </div>
-          <div class="flex items-center gap-4 mb-4">
+          <div class="flex items-center gap-4 mb-4 animate-slide-in" style="animation-delay: 0.4s">
             <UIcon name="line-md:cog" class="size-8" />
             <ULink to="/settings" class="text-2xl">Settings</ULink>
           </div>
-          <div class="flex items-center gap-4 mb-4">
+          <div class="flex items-center gap-4 mb-4 animate-slide-in" style="animation-delay: 0.6s">
             <UIcon name="line-md:star" class="size-8" />
             <ULink to="/rewards" class="text-2xl">Rewards & Badges</ULink>
           </div>
-          <div class="flex items-center gap-4 mb-4">
+          <div class="flex items-center gap-4 mb-4 animate-slide-in" style="animation-delay: 0.8s">
             <UIcon name="line-md:log-out" class="size-8" />
             <ULink to="/" class="text-2xl">Log out</ULink>
           </div>
