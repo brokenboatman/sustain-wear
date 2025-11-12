4173765bcd50831e358196e8d06c1faeb0458c15
NoelKBoyd <noel.boyd01@gmail.com>
2025-11-12 16:54:43 +0000

    Completed unauthorised view

--- src/router/index.js
+++ src/router/index.js
@@ -5,41 +5,62 @@ import RegisterView from "../views/RegisterView.vue";
 import AccountView from "../views/AccountView.vue";
 import RewardsView from "../views/RewardsView.vue";
 import SettingsView from "../views/SettingsView.vue";
+import UnauthorisedView from "@/views/UnauthorisedView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: RegisterView,
+  },
+  {
+    path: "/account",
+    name: "account",
+    component: AccountView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/rewards",
+    name: "rewards",
+    component: RewardsView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/settings",
+    name: "settings",
+    component: SettingsView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/Unauthorized",
+    name: "unauthorized",
+    component: UnauthorisedView,
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/login",
-      name: "login",
-      component: LoginView,
-    },
-    {
-      path: "/register",
-      name: "register",
-      component: RegisterView,
-    },
-    {
-      path: "/account",
-      name: "account",
-      component: AccountView,
-    },
-    {
-      path: "/rewards",
-      name: "rewards",
-      component: RewardsView,
-    },
-    {
-      path: "/settings",
-      name: "settings",
-      component: SettingsView,
-    },
-  ],
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem("token");
+  if (to.meta.requiresAuth && !token) {
+    next("/unauthorized");
+  } else {
+    next();
+  }
 });
 
 export default router;
