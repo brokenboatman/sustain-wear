7265017916c0f836cb36c925a59d0d24939a9864
Fleester <reece.g1611@gmail.com>
2025-11-12 16:45:13 +0000

    different approach

--- src/Components/DonationList.vue
+++ src/Components/DonationList.vue
@@ -1,41 +1,92 @@
+<script setup lang="ts">
+import { h, resolveComponent } from 'vue'
+import type { TableColumn } from '@nuxt/ui'
+import { ref } from 'vue'
+
+const UBadge = resolveComponent('UBadge')
+
+type Donation = {
+  id: string
+  imageRef: string
+  name: string
+  status: 'In transit' | 'Arrived at final depot' | 'On its way'
+}
+
+const data = ref<Donation[]>([
+  {
+    id: '001',
+    imageRef: "[image ref here]",
+    name: "T-Shirt",
+    status: 'In transit',
+  },
+  {
+    id: '002',
+    imageRef: "[image ref here]",
+    name: "Beige trousers",
+    status: 'Arrived at final depot',
+  },
+  {
+    id: '003',
+    imageRef: "[image ref here]",
+    name: "Floral dress",
+    status: 'On its way',
+  }
+])
+
+const columns: TableColumn<Donation>[] = [
+  {
+    accessorKey: 'id',
+    header: 'ID',
+    cell: ({ row }) => `#${row.getValue('id')}`,
+    meta: {
+      class: {
+        th: 'text-left font-bold',
+        td: 'text-left text-s'
+      }
+    },
+  },
+  {
+    accessorKey: 'imageRef',
+    header: 'Image',
+    cell: ({ row }) => `${row.getValue('imageRef')}`,
+    meta: {
+      class: {
+        th: 'text-left font-bold',
+        td: 'text-left text-s'
+      }
+    },
+  },
+  {
+    accessorKey: 'name',
+    header: 'Name',
+    cell: ({ row }) => `${row.getValue('name')}`,
+    meta: {
+      class: {
+        th: 'text-left font-bold',
+        td: 'text-left text-s'
+      }
+    },
+  },
+  {
+    accessorKey: 'status',
+    header: 'Status',
+    cell: ({ row }) => {
+
+      return h(UBadge, { class: 'capitalize', variant: 'subtle' }, () =>
+        row.getValue('status')
+      )
+    },
+    meta: {
+      class: {
+        th: 'text-right font-bold',
+        td: 'text-right'
+      }
+    },
+  }
+]
+</script>
+
 <template>
-    <UContainer class="Donation">
-        <div class="ImageBox">
-            <p>Image Goes Here</p>
-        </div>
-        <div class="NameBox">
-            <p>Donation 1</p>
-        </div>
-        <div class="StatusBox"> 
-            <UIcon name="i-lucide-lightbulb" class="size-5" />
-            <p>In Transit</p>
-        </div>
-        
-    </UContainer>
+  <UTable :data="data" :columns="columns" class="flex-1, color-default" />
 </template>
 
-<style>
-.Donation {
-    display: flex;
-    background-color: #666666;
-    border-radius: 8px;
-    justify-content: space-evenly;
-    height: 100px;
-    
-}
-
-.ImageBox {
-    display: flex;
-    width: 30%;
-}
-
-.NameBox {
-    display: flex;
-    width: 30%;
-}
-
-.StatusBox {
-    display: flex;
-    width: 40%;
-}
-</style>
