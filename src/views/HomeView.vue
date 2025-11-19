c7cf6b9fcca3fe1918a5fb3f5f337e702e0c6b4b
Fleester <reece.g1611@gmail.com>
2025-11-19 15:31:50 +0000

    quick fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,13 @@
 <script setup>
 import Sidebar from '../components/Sidebar.vue';
 import BarChart from '../components/BarChart.vue';
-import DonationList from '../components/DonationList.vue';
+import DonationTab from '../components/DonationTab.vue';
 import Header from "@/components/Header.vue";
 import NewDonationDialog from "./NewDonationDialog.vue";
 </script>
 
 <template>
   <main>
-    <header class="border-b border-inverted mb-4 pb-2">
-      <div class="flex align-center justify-between p-4">
-        <div class="flex items-center">
-          <h1 class="text-default text-2xl animate-slide-in font-brand">SustainWear</h1>
-        </div>
-        <div class="flex items-end justify-end">
-          <UButton icon="line-md:bell" color="neutral" variant="ghost" />
-          <sidebar />
-        </div>
-      </div>
-    </header>
     <DonationTab />
     <BarChart />
     <NewDonationDialog />
