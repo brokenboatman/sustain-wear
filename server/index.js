2f24be04535951fc42efc538be541372db25e7bc
Asher Denno <brokenboatman@gmail.com>
2025-12-02 19:00:18 +0000

    - Added API endpoint for fetching staff donations that need to be approved

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import fetchDonationsRoutes from "./routes/fetch-donations.js";
+import fetchStaffDonationsRoutes from "./routes/staff-fetch-donations.js";
 import addDonationRoutes from "./routes/add-donation.js";
 import donationOptionsRoutes from "./routes/donation-options.js";
 import dotenv from "dotenv";
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use("/api/auth", authRoutes);
 app.use("/api/fetch-donations", fetchDonationsRoutes);
+app.use("/api/fetch-staff-donations", fetchStaffDonationsRoutes);
 app.use("/api/add-donation", addDonationRoutes);
 app.use("/api/donation-options", donationOptionsRoutes);
 
