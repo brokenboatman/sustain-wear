0cfc7b3bab4fb0aa0bbe6251d1842a0a9de58e11
Asher Denno <brokenboatman@gmail.com>
2025-12-05 15:55:43 +0000

    Added some test images for implementing an image filestore Removed unused / unnecessary imports

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,4 @@
 <script setup>
-import Sidebar from "../Components/Sidebar.vue";
 import BarChart from "../components/BarChart.vue";
 import DonationTab from "../components/DonationTab.vue";
 import Header from "@/components/Header.vue";
